57ab0239ca1ff8e2f1a590d70511aa69819d8ebb
Mohamed Aymane Farhi <aymenfarhi.25@gmail.com>
2025-05-16 02:11:17 +0300

    Add download directory as an argument

--- download_data.sh
+++ download_data.sh
@@ -1,7 +1,16 @@
-# Script to download the raw data. Archives containing audio data are in audio_links.txt, and the text data is in text_link.txt.
+#!/bin/bash
+
+# Check if the directory argument is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <directory>"
+    exit 1
+fi
+
+# Directory containing the txt files
+DIR=$1
 
 # Download audio data
-wget -i audio_links.txt --content-disposition -P raw_audio/
+wget -i "$DIR/audio_links.txt" --content-disposition -P "$DIR/raw_audio/"
 
 # Download text data
-wget -i text_link.txt --content-disposition -P raw_text/
+wget -i "$DIR/text_link.txt" --content-disposition -P "$DIR/raw_text/"
